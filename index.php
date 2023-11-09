6280c4cdfa8d8ccd6f966fa86750d909f30727ef
Belchenkov <u608110@gmail.com>
2023-11-09 09:41:34 +0400

    Send Keyboard from bot

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+// https://api.telegram.org/bot6594948161:AAFCNZxQym04wZEs9OE7CPQZtr3IXjvI-yU/setWebhook?url=https://pet-projects.sharedwithexpose.com/php/eshop_telegram
 error_reporting(-1);
 ini_set('display_errors', 0);
 ini_set('log_errors', 'on');
@@ -10,3 +10,37 @@ require_once __DIR__ . '/config.php';
 $phrases = require_once __DIR__ . '/phrases.php';
 require_once __DIR__ . '/keyboards.php';
 require_once __DIR__ . '/functions.php';
+
+/**
+ * @var $keyboard1
+ * @var $inline_keyboard1
+ */
+
+$telegram = new \Telegram\Bot\Api(TOKEN);
+$update = $telegram->getWebhookUpdate();
+
+debug($update);
+
+$chat_id = $update['message']['chat']['id'] ?? 0;
+$text = $update['message']['text'] ?? '';
+$name = $update['message']['from']['first_name'] ?? 'Guest';
+
+if (!$chat_id) {
+    die;
+}
+
+if ($text === '/start') {
+    $telegram->sendMessage([
+        'chat_id' => $chat_id,
+        'text' => sprintf($phrases['start'], $name),
+        'parse_mode' => 'HTML',
+        'reply_markup' => new \Telegram\Bot\Keyboard\Keyboard($keyboard1),
+    ]);
+
+    $telegram->sendMessage([
+        'chat_id' => $chat_id,
+        'text' => $phrases['inline_keyboard'],
+        'parse_mode' => 'HTML',
+        'reply_markup' => new \Telegram\Bot\Keyboard\Keyboard($inline_keyboard1),
+    ]);
+}
--- keyboards.php
+++ keyboards.php
@@ -1,1 +1,33 @@
 <?php
+
+/**
+ * @var $phrases
+ */
+
+$keyboard1 = [
+    'keyboard' => [
+        [
+            [
+                'text' => $phrases['btn_subscribe'],
+                'web_app' => [
+                    'url' => WEB_APP1
+                ],
+            ]
+        ]
+    ],
+    'resize_keyboard' => true,
+    'input_field_placeholder' => $phrases['select_btn'],
+];
+
+$inline_keyboard1 = [
+    'inline_keyboard' => [
+        [
+            [
+                'text' => $phrases['inline_btn'],
+                'web_app' => [
+                    'url' => WEB_APP2
+                ],
+            ]
+        ]
+    ],
+];
